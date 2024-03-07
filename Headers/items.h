3cf1bbaeb71b463680c02aed6a9eb2069b90c7d8
123100123 <146941456+123100123@users.noreply.github.com>
2024-03-07 16:52:57 +0330

    weapons

--- Headers/items.h
+++ Headers/items.h
@@ -1,7 +1,73 @@
 #pragma once
 
 
-class items {
+class Item {
+protected:
+    string Name;
+    int Capacity;
+    int Price;
+public:
+    item(string name,int capacity);
+    void setCapacity(int capacity);
+    void drop();
+    void pick();
+};
+
+//--------------------------------
+class Weapon: public Item{
+protected:
+    int Damage;
+    int EnergyNeeded;
+    int AttackRange;
+public:
+    Weapon(string name,int capacity,int damage);
+    void setDamage(int damage);
+    void Attack(); // takes an enemy and calls its TakeDamage function
+    void decreaseEnergy(); //decreases player's energy
+};
+class Gun:public Weapon{
+private:
+    int ammo;
+public:
+    void Shoot() override; //checks the range for the attack and decreases the ammo
+    void setRange(int range);
+};
+
+class Shotgun:public Gun{
+public:
+    void Shoot(); // checks the range and deals damage based on it
+};
+
+class Snipe: public Gun{
+public:
+    void shoot(); // takes multiple enemies and attacks them all
+};
+
+class Rifle: public Weapon{
+public:
+    void Shoot(); // takes some enemies and the damage is divided between
+};
+
+class coldWeapon: public Weapon{
+public:
+    void throw(); //throws the coldWeapon and loses it
+};
+
+class Throwable: public Weapon {
+private:
+
+public:
+    void throw();
+};
+
+class Bomb : public Throwable{
+private:
+    int damage;
+public:
+    void
 };
 
 
+//--------------------------------
+
+class Consu
