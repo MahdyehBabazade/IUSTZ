93eccb0eb92dd9112b24dc13b085f4f56b9f8157
amir-rahmani84 <152492156+amir-rahmani84@users.noreply.github.com>
2024-03-17 01:47:07 +0330

    Update Tester.cpp

--- Tester.cpp
+++ Tester.cpp
@@ -14,14 +14,14 @@ vector<string> ShuffleVec(vector<string> vec){
 }
 
 void check_attack(){
-    Weapon* weapon = new Snipe("sniper",5,20,20,4,2);
+    Weapon* weapon = new Snipe("sniper",5,20,20, 4, 1 , 2 , 1);
     Consumable*  consumable = new Consumable("hiHPPoition" , "HPPotion" , 1 , 10 , 30); 
     vector<pair<Item*,int>> items = {make_pair(consumable , 1),make_pair(weapon,1)};
     vector<pair<Weapon* , int>> Weapons = {make_pair(weapon,1)};
     vector<pair<Consumable* , int>> Consumables = {make_pair(consumable , 1)};
     vector<Equipment*> Equipments;
-    Player* player = new Player("O",100,100,0,100,0,100,40,50,items,Weapons,Equipments);
-    HumanEnemy* enemy = new HumanEnemy(30,30,0,"enemy1",0,items,Weapons,Equipments,Weapons,Consumables);
+    Player* player = new Player("O",100,100,1.0,100,0,100,40,50,items,Weapons,Consumables , Equipments);
+    HumanEnemy* enemy = new HumanEnemy("enemy1",30,30,0.0,0,items,Weapons,Equipments,Consumables);
 }
 
 int main(){
