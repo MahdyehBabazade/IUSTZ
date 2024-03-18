681ed9612a20ec8bbb9406068d86cb37912ef80e
MahdyehBabazade <babazademahdye@gmail.com>
2024-03-19 01:33:05 +0330

    updated tester3

--- Tester3.cpp
+++ Tester3.cpp
@@ -20,80 +20,84 @@ void check_dialogues(){
 
     HumanEnemy enemy1("C", 100, 100, items, weapons, equipments, consumables);
 
-    cout << "Choose the character you want to talk: "<< endl;
-    cout << "1. Shopkeeper" << endl << "2. Medic" << endl << "3. Human Enemy";
-
-    int n;
-    cin >> n;
-    switch (n)
+    cout << "Choose the character you want to talk: ";
+    while (true)
     {
-    case 1:
-        cout << "1. Hi \n"
-                "2. Bye \n"
-                "3. Sell \n"
-                "4. Buy \n"
-                "5. No Money \n";
+        cout << endl << "1. Shopkeeper" << endl << "2. Medic" << endl << "3. Human Enemy" << endl << "4. Exit" << endl;
+        int n;
+        cin >> n;
         int choice;
-        cin >> choice;
-        switch (choice)
+        switch (n)
         {
         case 1:
-            shopkeeper1.HiDialogue();
-            break;
+            cout << "1. Hi \n"
+                    "2. Bye \n"
+                    "3. Sell \n"
+                    "4. Buy \n"
+                    "5. No Money \n";
+            int choice;
+            cin >> choice;
+            switch (choice)
+            {
+                case 1:
+                    shopkeeper1.HiDialogue();
+                    break;
+                case 2:
+                    shopkeeper1.ByeDialogue();
+                    break;
+                case 3:
+                    shopkeeper1.SellDialogue(weapon);
+                    break;
+                case 4: 
+                    shopkeeper1.BuyDialogue(weapon);
+                    break;
+                case 5: 
+                    shopkeeper1.NoMoneyDialogue();
+                default:
+                    break;
+            }
         case 2:
-            shopkeeper1.ByeDialogue();
+            cout << "1. Hi \n"
+                    "2. Bye \n"
+                    "3. Heal \n"
+                    "4. No Money \n";
+            cin >> choice;
+            switch (choice)
+            {
+                case 1:
+                    medic1.HiDialogue();
+                    break;
+                case 2:
+                    medic1.ByeDialogue();
+                    break;
+                case 3:
+                    medic1.HealDialogue();
+                    break;
+                case 4:
+                    medic1.NoMoneyDialogue();
+                default:
+                    break;
+            }
             break;
         case 3:
-            shopkeeper1.SellDialogue(weapon);
-            break;
-        case 4: 
-            shopkeeper1.BuyDialogue(weapon);
-            break;
-        case 5: 
-            shopkeeper1.NoMoneyDialogue();
-        default:
-            break;
-        }
-        break;
-    case 2:
-        cout << "1. Hi \n"
-                "2. Bye \n"
-                "3. Heal \n"
-                "4. No Money \n";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            medic1.HiDialogue();
-            break;
-        case 2:
-            medic1.ByeDialogue();
-            break;
-        case 3:
-            medic1.HealDialogue();
+            cout << "1. Rajaz Khani \n";
+            cin >> choice;
+            switch (choice)
+            {
+                case 1:
+                    enemy1.RajazKhani();
+                    break;
+                default:
+                    break;
+            }
             break;
         case 4:
-            medic1.NoMoneyDialogue();
+            exit(0);
+            break; 
         default:
             break;
-        }
-        break;
-    case 3:
-        cout << "1. Rajaz Khani \n";
-        int choice1;
-        cin >> choice1;
-        switch (choice1)
-        {
-        case 1:
-            enemy1.RajazKhani();
-            break;
-        default:
-            break;
-        }
-      break;
-    default:
-        break;
-    }    
+        }    
+    }
 }
 
 int main(){
