de37fd204447bdd0ae58be88ad867454cbe20cf9
bahar-jafarnejad <jafarnejadbahar3@gmail.com>
2024-03-20 01:57:36 +0330

    debug map class

--- Cpps/map.cpp
+++ Cpps/map.cpp
@@ -1,17 +1,17 @@
 #include "../Headers/map.h"
 #include <iostream>
 
-vector<int> PathFinding1(vector<int> Path1);
+vector<int> PathFinding1();
 
-vector<int> PathFinding2(vector<int> Path2);
+vector<int> PathFinding2(vector<int> path1);
 
-vector<int> PathFinding3(vector<int> Path3);
+vector<int> PathFinding3(vector<int> path1,vector<int> path2);
 
-vector<int> PathFinding4(vector<int> Path4);
+vector<int> PathFinding4(vector<int> path1,vector<int> path2,vector<int>path3);
 
-Map::Map(int floor,vector<int> path1, vector<int> path1, vector<int> path1 ,vector<int> path1,vector<vector<Encounter*>> generateEncounter)
-: Floor(floor), Path1(path1), Path2(path2), Path3(path3) ,Path4(path4) ,GenerateEncounter(generateEncounter){}
+string Showmap(vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4,vector<vector<Encounter*>> generateEncounter);
 
-string Map::ShowMap(vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4,vector<vector<Encounter*>> generateEncounter){
-    
-}
+Map::Map(int floor,string map,vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4 ,vector<vector<Encounter*>> generateEncounter)
+: Floor(floor),ShowMap(map), Path1(path1), Path2(path2), Path3(path3) ,Path4(path4) ,GenerateEncounter(generateEncounter){}
+
+string Map::getMap() { return ShowMap;}
--- Headers/map.h
+++ Headers/map.h
@@ -8,12 +8,13 @@ using namespace std;
 class Map{
 private:
     int Floor;
+    string ShowMap;
     vector<int> Path1;
     vector<int> Path2;
     vector<int> Path3;
     vector<int> Path4;
     vector<vector<Encounter*>> GenerateEncounter;
 public:
-    Map(int floor,vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4,vector<vector<Encounter*>> generateEncounter);
-    string ShowMap(vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4,vector<vector<Encounter*>> generateEncounter);
+    string getMap();
+    Map(int floor,string map,vector<int> path1, vector<int> path2, vector<int> path3 ,vector<int> path4,vector<vector<Encounter*>> generateEncounter);
 };
